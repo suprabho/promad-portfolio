27002ed65dc439a0a186d4c26e817cb9d3735521
suprabho <suprabho13@gmail.com>
2025-06-20 17:30:57 +0530

    mobile keyboard

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -14,7 +14,7 @@ export default function ChatPage() {
   }
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-dvh bg-background">
       <Header />
       {isChatOpen && <Chat onClose={closeChat} />}
     </div>
